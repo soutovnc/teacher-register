c70c95d2dc1f2d1c4b9fcaf4c177ae58cd27b165
Vinicius <viniciusdesouto@gmail.com>
2020-07-31 19:56:52 -0300

    add new properties

---
+++ public/scripts.js
@@ -0,0 +1,13 @@
+const modalOverlay = document.querySelector('.modal-overlay')
+const cards = document.querySelectorAll('.card')
+const modal = document.querySelector('.modal')
+
+for (let card of cards) {
+    card.addEventListener("click", function() {
+        const pageId = card.getAttribute("id")
+        window.location.href = `/course?id=${pageId}`
+
+    })
+}
+
+
---
+++ routes.js
@@ -0,0 +1,25 @@
+const express = require('express');
+const routes = express.Router();
+
+
+routes.get('/', function(req, res) {
+    return res.redirect("/teachers")
+})
+
+routes.get('/teachers', function(req, res) {
+    return res.render("teachers/index")
+})
+
+routes.get('/teachers/create', function(req, res) {
+    return res.render("teachers/create")
+})
+
+routes.post('/teachers', function(req, res) {
+    return res.send("recebido")
+})
+
+routes.get('/members', function(req, res) {
+    return res.send("students")
+})
+
+module.exports = routes
