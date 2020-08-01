dc7c5b9fa190d17973979c67f3f49e219ec1c07e
Vinicius <viniciusdesouto@gmail.com>
2020-08-01 03:04:36 -0300

    update this files

--- routes.js
+++ routes.js
@@ -1,6 +1,6 @@
 const express = require('express');
 const routes = express.Router();
-
+const teachers = require('./teachers')
 
 routes.get('/', function(req, res) {
     return res.redirect("/teachers")
@@ -14,17 +14,7 @@ routes.get('/teachers/create', function(req, res) {
     return res.render("teachers/create")
 })
 
-routes.post('/teachers', function(req, res) {
-    const keys = Object.keys(req.body)
-
-    for (key of keys) {
-        if (req.body[key] == "") {
-            return res.send('Please, fill all fields')
-        }
-    }
-    
-    return res.send(req.body)
-})
+routes.post('/teachers', teachers.post)
 
 routes.get('/members', function(req, res) {
     return res.send("students")
---
+++ teachers.js
@@ -0,0 +1,25 @@
+const fs = require('fs')
+const data = require('./data.json')
+
+exports.post = function(req, res) {
+    const keys = Object.keys(req.body)
+
+    for (key of keys) {
+        if (req.body[key] == "") {
+            return res.send('Please, fill all fields')
+        }
+    }
+
+    req.body.birth = Date.parse(req.body.birth)
+    req.body.created_at = Date.now()
+    
+    data.teachers.push(req.body)
+
+    fs.writeFile("data.json", JSON.stringify(data, null, 2), function(err){
+        if (err) {
+            return res.send("Write file error!")
+        }
+        return res.redirect("/teachers")
+    })
+    return res.send(req.body)
+}
